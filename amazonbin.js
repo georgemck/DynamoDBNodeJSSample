c77fc4466034202f32bc1d239116bf1c67715a55
George <george@hookstream.com>
2020-06-11 17:35:08 -0700

    switching to AWS SDK for JSON transport in index.js and added example for Amazon

--- amazonbin.js
+++ amazonbin.js
@@ -6,38 +6,40 @@ const region = 'us-west-2';
 AWS.config.update({
     region: region
 });
+
 //https://github.com/awsdocs/aws-doc-sdk-examples/blob/master/javascript/example_code/dynamodb/ddbdoc_get.js
 // Create DynamoDB document client
 var docClient = new AWS.DynamoDB.DocumentClient({
     apiVersion: '2012-08-10'
 });
 
-const selenajson = require("./selena.json");
-
 ///////////////////CODE EXECUTION FLOW//////////////////////////////
 
 console.log("program start");
 
-for (const element of selenajson.results) {
-
-    var params = {
-        TableName: 'AppleMusic',
-        "Item": element
-    };
-
-    if (!element.artistId) {
-        return;
-    }
-
-    // INSERT DATA INTO DYNAMODB
-    docClient.put(params, function (err, data) {
-        if (err) {
-            console.log(err, "Did not add  --> " + element.artistName + ": " + element.trackName);
-        } else {
-            console.log(data, "Empty brackets {} means no error. Added " + element.artistName + ": " + element.trackName);
-        }
-    });
-
+var file = null;
+if (process.argv.length > 2) {
+    file = [process.argv[2]][0]
 }
+if (file === null) {
+    console.log("specifify an image number", file);
+    process.exit(1);
+}
+
+const datajson = require("./data/" + file + ".json");
+var params = {
+    TableName: 'amazonbins',
+    "Item": datajson
+};
+
+// INSERT DATA INTO DYNAMODB
+docClient.put(params, function (err, data) {
+    if (err) {
+
+        console.log(err, "Did not add " + file);
+    } else {
+        console.log(data, "Empty brackets {} means no error. Added " + file);
+    }
+});
 
 console.log("program end");
